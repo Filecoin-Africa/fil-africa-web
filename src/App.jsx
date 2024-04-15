ff873df807efafe26baa9c3881b29b9e806d431e
alabo-excel <iamalaboexcel@gmail.com>
2024-04-15 14:13:37 +0100

    homepage sections commit

--- src/App.jsx
+++ src/App.jsx
@@ -6,22 +6,27 @@ function App() {
   return (
     <>
       <HeaderNav />
-      <section>
-        <div className="w-[50%] text-center mx-auto py-44">
-          <h1 className="text-5xl font-medium">We are the Filecoin community in Africa</h1>
-          <p className="text-sm my-2">Build with us. we are growing the Filecoin community in Africa</p>
-          <button className="text-xs my-3 rounded-full p-5 px-10 bg-[#0F62FE]">Join Our Community</button>
-        </div>
-      </section>
-      <div className="m-20 flex border border-[#AAADB3] rounded-xl justify-between p-6">
-        <div className="w-[40%]">
-          <h1 className="text-3xl w-[70%] my-3">Community and Engagement</h1>
-          <p className="text-xs my-4">Join our vibrant community of developers, enthusiasts, and experts passionate about decentralized storage and blockchain technology. Participate in workshops, events, and hackathons designed to promote collaboration and knowledge sharing.</p>
-          <button className="text-xs my-3 rounded-full p-5 px-10 border border-[#0F62FE] text-[#0F62FE]">Join Our Community</button>
+      <div className="relative">
+        <section>
+          <div className="w-[50%] text-center mx-auto py-44">
+            <button className="bg-btn p-2 px-6 text-xs mb-4 rounded-full">FIL _ AFRICA</button>
+            <h1 className="text-5xl font-medium">We are the Filecoin community in Africa</h1>
+            <p className="text-sm my-2">Build with us. we are growing the Filecoin community in Africa</p>
+            <button className="text-xs my-3 rounded-full p-5 px-10 bg-[#0F62FE]">Join Our Community</button>
+          </div>
+        </section>
+        <img className="h-[100vh] top-96 absolute w-full" src="/images/bg.png" alt="" />
+        <div className="mx-20 flex relative glass my-20 border border-[#AAADB3] rounded-xl justify-between p-6">
+          <div className="w-[40%]">
+            <h1 className="text-3xl w-[70%] my-3">Community and Engagement</h1>
+            <p className="text-xs my-4">Join our vibrant community of developers, enthusiasts, and experts passionate about decentralized storage and blockchain technology. Participate in workshops, events, and hackathons designed to promote collaboration and knowledge sharing.</p>
+            <button className="text-xs my-3 rounded-full p-5 px-10 border border-[#0F62FE] text-[#0F62FE]">Join Our Community</button>
 
-        </div>
-        <div className="w-[40%]">
-          <img className="h-80 ml-auto" src="/images/about.png" alt="" />
+          </div>
+          <img src="/images/round.png" className="absolute bottom-0 left-80" alt="" />
+          <div className="w-[40%]">
+            <img className="h-80 ml-auto" src="/images/about.png" alt="" />
+          </div>
         </div>
       </div>
       <div className="mx-32 text-center">
@@ -30,18 +35,19 @@ function App() {
         <img className="my-4" src="/Images.png" alt="" />
         <p className="w-1/2 text-[10px] mx-auto">Join our vibrant community of developers, enthusiasts, and experts passionate about decentralized storage and blockchain technology. Participate in workshops, events, and hackathons designed to promote collaboration and knowledge sharing.</p>
       </div>
+      <div className="relative">
+        <img className="h-[100vh] w-full" src="/images/bg.png" alt="" />
+        <div className="m-20 glass absolute top-20 flex border border-[#AAADB3] rounded-xl justify-between p-6">
+          <div className="w-[35%]">
+            <h1 className="text-3xl w-[70%] my-3">Contribute to  Open-Source</h1>
+            <p className="text-xs my-4">Join our vibrant community of developers, enthusiasts, and experts passionate about decentralized storage and blockchain technology. Participate in workshops, events, and hackathons designed to promote collaboration and knowledge sharing.</p>
+            <button className="text-xs my-3 rounded-full p-5 px-10 border border-[#0F62FE] text-[#0F62FE]">Join Our Community</button>
 
-      <div className="m-20 flex border border-[#AAADB3] rounded-xl justify-between p-6">
-        <div className="w-[35%]">
-          <h1 className="text-3xl w-[70%] my-3">Contribute to  Open-Source</h1>
-          <p className="text-xs my-4">Join our vibrant community of developers, enthusiasts, and experts passionate about decentralized storage and blockchain technology. Participate in workshops, events, and hackathons designed to promote collaboration and knowledge sharing.</p>
-          <button className="text-xs my-3 rounded-full p-5 px-10 border border-[#0F62FE] text-[#0F62FE]">Join Our Community</button>
-
+          </div>
+          <div className="w-[40%]"></div>
         </div>
-        <div className="w-[40%]"></div>
       </div>
-
-      <section className="text-center my-20 mx-40">
+      <section className="text-center my-10 mx-40">
         <h1 className="text-4xl">Meet Us</h1>
         <p className="text-xs">Here is a gallery of our amazing community of people</p>
         <div className="flex mt-6 justify-between flex-wrap">
--- src/components/HeaderNav.jsx
+++ src/components/HeaderNav.jsx
@@ -1,7 +1,7 @@
 
 const HeaderNav = () => {
   return (
-    <div className="flex p-4 px-20 justify-between fixed top-0 w-full">
+    <div className="flex p-4 z-10 px-20 justify-between fixed top-0 w-full">
         <h1 className="text-xl font-bold">#fil africa</h1>
         <div className="flex text-xs justify-between w-[30%]">
           <p>About Us</p>
