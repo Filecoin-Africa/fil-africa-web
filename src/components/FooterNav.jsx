626fd2bd5edfea3267a1d8405734326c2c2ab97f
alabo-excel <iamalaboexcel@gmail.com>
2024-04-15 13:55:40 +0100

    first commit

---
+++ src/components/FooterNav.jsx
@@ -0,0 +1,24 @@
+
+const FooterNav = () => {
+  return (
+    <div className="p-3">
+      <footer className="bg-[#15151C] rounded-xl p-8">
+        <div className="text-center">
+          <p className="text-xl font-medium">Join Our Community</p>
+          <p className="text-[10px] w-[20%] my-2 mx-auto">Let us unleash our creativity and
+            expertise to create designs that deliver
+            extraordinary results.</p>
+          <button className="text-xs my-3 rounded-full p-4 px-10 bg-[#0F62FE]">Join Our Community</button>
+
+        </div>
+        <div className="border-t pt-5 border-[#1D2739] flex justify-between mt-5">
+          <p className="text-xs">#fil africa</p>
+          <p className="text-[9px]">© 2024 FIL Africa. All rights reserved.</p>
+          <div></div>
+        </div>
+      </footer>
+    </div>
+  );
+};
+
+export default FooterNav;
---
+++ src/components/HeaderNav.jsx
@@ -0,0 +1,15 @@
+
+const HeaderNav = () => {
+  return (
+    <div className="flex p-4 px-20 justify-between fixed top-0 w-full">
+        <h1 className="text-xl font-bold">#fil africa</h1>
+        <div className="flex text-xs justify-between w-[30%]">
+          <p>About Us</p>
+          <p>Filecoin</p>
+          <p>Contact Us</p>
+        </div>
+    </div>
+  );
+};
+
+export default HeaderNav;
